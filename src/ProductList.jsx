58c5d6f42e3552f77c60a5dfa80f3091186f6c74
TechGracia <graciajopson@gmail.com>
2024-10-15 11:00:03 -0400

    CHNAGES ARE DONE

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -296,7 +296,13 @@ const handleAddToCart = (product) => {
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-cost">{plant.cost}</div>
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        <button
+                                            className="product-button"
+                                            onClick={() => handleAddToCart(plant)}
+                                            disabled={addedToCart[plant.name]}
+                                        >
+                                            {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
+                                        </button>
                                     </div>
                                 ))}
                             </div>
@@ -304,7 +310,7 @@ const handleAddToCart = (product) => {
                     ))}
                 </div>
             ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
+                <CartItem onContinueShopping={() => setShowCart(false)} />
             )}
         </div>
     );
